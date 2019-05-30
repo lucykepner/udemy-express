a1d783231b9f0551301ac63fc9d823a45ccfdd95
Karen Kepner <kkepner14@gmail.com>
2019-05-30 08:37:05 -0600

    Add first get statement to app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 let express = require('express');
 let app = express();
-let port = 4000
+let port = 4000;
+
+//define first route
+app.get("/", function(req, res) { //req = request, res = response
+  res.send("Hi there, welcome to my assignment!");
+});
+
+//app.get("/speak/:pig")
+
+//tell Express to listen for requests (start server)
+app.listen(port, () => console.log(`app.js listening on port ${port}`));
+
